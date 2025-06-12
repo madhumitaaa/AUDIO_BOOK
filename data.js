1426e70218963b902e566e6f78d05590d025fa5c
madhumitaaa <madhumita.mandal.ee26@heritageit.edu.in>
2025-06-13 02:28:57 +0530

    added data.js modified html and css

---
+++ data.js
@@ -0,0 +1,76 @@
+const songs = [
+  {
+    id: "1X2GOpyrC32Cvf-EHEVUNPrmja6fNSUGx",
+    title: "dhadak",
+    artist: "Ajay Atul",
+    image: "dhadak.jpg"
+  },
+  {
+    id: "1_g1WFJ9C4bBTJ99CegDoHUi-wE2k-3vU",
+    title: "Besos",
+    artist: "Juanes",
+    image: "besos.jpg"
+  },
+  {
+    id: "1a7VA0IZDoXldCdTyToi6k5_RSM6TveLd",
+    title: "Ye Ishq Hai",
+    artist: "Shreya Ghoshal",
+    image: "yeishqhai.jpg"
+  },
+  {
+    id: "1zOv56Hopfh6sTJEGav7M2E8Go3G6_58W",
+    title: "Nagada Sang Dhol",
+    artist: "Shreya Ghoshal",
+    image: "nagada.jpg"
+  },
+  {
+    id: "1Y8W8s9e7lUOpdWViAilGmLxtkLHMWaZg",
+    title: "Music",
+    artist: "Unknown Artist",
+    image: "music.jpg"
+  },
+  {
+    id: "1gLyNv8GsiuI_PTa14GBTzOLy5v7jkvzg",
+    title: "Balam Pichkari",
+    artist: "Shalmali Kholgade & Vishal Dadlani",
+    image: "balam.jpg"
+  },
+  {
+    id: "1M5pwHtth0CfBS0yY2M0hTQ9a8BaE31fq",
+    title: "Tera Chehra",
+    artist: "Arijit Singh",
+    image: "terachehra.jpg"
+  },
+  {
+    id: "1aHDpl4Pd2JDrM0QM3CtPNn1zr-5p0-XV",
+    title: "Radha",
+    artist: "Shreya Ghoshal & Vishal–Shekhar",
+    image: "radha.jpg"
+  },
+  {
+    id: "19k_V9mv-FiGfaDi52cgcvslzrvirJOIF",
+    title: "Jhalla Wallah",
+    artist: "Shreya Ghoshal",
+    image: "jhalla.jpg"
+  },
+  {
+    id: "1zKr-aGCK2fZNJ6KCcJ02lz1_40CB_zDC",
+    title: "Sad Emotional Piano",
+    artist: "Instrumental",
+    image: "piano.jpg"
+  },
+  {
+    id: "1td4_D99h5LA0Wu8amF_s8k3ZxdaDKDNa",
+    title: "Angels by My Side",
+    artist: "Bensound",
+    image: "angels.jpg"
+  },
+  {
+    id: "1jBu7zIe09vNc4j8K287wgQqWswLpW0o6",
+    title: "Tera Yaar Hoon Main",
+    artist: "Arijit Singh",
+    image: "terayaar.jpg"
+  }
+];
+
+export default songs;
