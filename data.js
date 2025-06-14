580ff639260659417a9c3b1001249462bec44230
madhumitaaa <madhumita.mandal.ee26@heritageit.edu.in>
2025-06-14 08:36:16 +0530

    added song.html and song.css added previous nect playable buttons

--- data.js
+++ data.js
@@ -1,76 +1,79 @@
 const songs = [
+  
+  
   {
     id: "1X2GOpyrC32Cvf-EHEVUNPrmja6fNSUGx",
     title: "dhadak",
     artist: "Ajay Atul",
-    image: "dhadak.jpg"
+    image: "https://m.media-amazon.com/images/I/71ZB-oICq2L._AC_UF1000,1000_QL80_.jpg"
   },
   {
     id: "1_g1WFJ9C4bBTJ99CegDoHUi-wE2k-3vU",
     title: "Besos",
     artist: "Juanes",
-    image: "besos.jpg"
+    image: "https://i.ytimg.com/vi/a52pZyr-gkw/maxresdefault.jpg"
   },
   {
     id: "1a7VA0IZDoXldCdTyToi6k5_RSM6TveLd",
     title: "Ye Ishq Hai",
     artist: "Shreya Ghoshal",
-    image: "yeishqhai.jpg"
+    image: "https://images.genius.com/155dd881e1f1d81245a2249d22ba1ee5.1000x1000x1.jpg"
   },
   {
     id: "1zOv56Hopfh6sTJEGav7M2E8Go3G6_58W",
     title: "Nagada Sang Dhol",
     artist: "Shreya Ghoshal",
-    image: "nagada.jpg"
+    image: "https://www.india.com/wp-content/uploads/2014/09/deep-01231654.jpg"
   },
   {
     id: "1Y8W8s9e7lUOpdWViAilGmLxtkLHMWaZg",
     title: "Music",
     artist: "Unknown Artist",
-    image: "music.jpg"
+    image: "https://static.vecteezy.com/system/resources/thumbnails/049/546/770/small_2x/stunning-high-resolution-nature-and-landscape-backgrounds-breathtaking-scenery-in-hd-free-photo.jpg"
   },
   {
     id: "1gLyNv8GsiuI_PTa14GBTzOLy5v7jkvzg",
     title: "Balam Pichkari",
     artist: "Shalmali Kholgade & Vishal Dadlani",
-    image: "balam.jpg"
+    image: "https://i1.sndcdn.com/artworks-000310163340-x0nh2l-t1080x1080.jpg"
   },
   {
     id: "1M5pwHtth0CfBS0yY2M0hTQ9a8BaE31fq",
     title: "Tera Chehra",
     artist: "Arijit Singh",
-    image: "terachehra.jpg"
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCIG6o6fCsjRBOk3n-Nj-EsTY9llj3pqUNFRXrEeM9Fup7tnrO81Mfi2CSxdZ4JAtPDpw&usqp=CAU"
   },
   {
-    id: "1aHDpl4Pd2JDrM0QM3CtPNn1zr-5p0-XV",
+    id:"1Z3WGNvoMhF0vdO0GqdZXJI53FeUKU_Ob",
     title: "Radha",
     artist: "Shreya Ghoshal & Vishal–Shekhar",
-    image: "radha.jpg"
+    image: "https://c-cdnet.cdn.smule.com/rs-s84/arr/7e/d6/6dcb3c6a-659e-42ac-9d01-bcc684d59ed1.jpg"
   },
   {
     id: "19k_V9mv-FiGfaDi52cgcvslzrvirJOIF",
     title: "Jhalla Wallah",
     artist: "Shreya Ghoshal",
-    image: "jhalla.jpg"
+    image: "https://www.yashrajfilms.com/images/default-source/Movies/Ishaqzaade/Ishaqzaade-Gallery/gauhar-khan-and-arjun-kapoor-in-ishaqzaade158a9ca026f56f7f9f64ff0000090313.jpg?sfvrsn=cc7ff3cc_2"
   },
   {
     id: "1zKr-aGCK2fZNJ6KCcJ02lz1_40CB_zDC",
     title: "Sad Emotional Piano",
     artist: "Instrumental",
-    image: "piano.jpg"
+    image: "https://img.freepik.com/premium-photo/piano-sitting-dock-sunset-beautiful-scenery-instrument_379823-13074.jpg"
   },
   {
     id: "1td4_D99h5LA0Wu8amF_s8k3ZxdaDKDNa",
     title: "Angels by My Side",
     artist: "Bensound",
-    image: "angels.jpg"
+    image: "https://upload.wikimedia.org/wikipedia/en/d/d4/Angels_on_My_Side.jpg"
   },
   {
     id: "1jBu7zIe09vNc4j8K287wgQqWswLpW0o6",
     title: "Tera Yaar Hoon Main",
     artist: "Arijit Singh",
-    image: "terayaar.jpg"
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuWMehMNbyOG_IhKsz6E5Nd6w0OsjL4BoMKoHfDv9XCjlMFVG4vL7vd32y8cAkWG84xoA&usqp=CAU"
   }
+ 
 ];
 
 export default songs;
